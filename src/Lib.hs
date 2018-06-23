ed29737ee46b87bce2e6c0d5b381fc869c1ce96a
noavarice <noavarice@gmail.com>
2018-06-23 16:14:27 +0300

    1 exercise done

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,16 @@
 module Lib
-    ( someFunc
+    ( fun1
+    , fun2
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- Exercise 1
+fun1 :: [Integer] -> Integer
+fun1 list = foldr (*) 1 $ map (subtract 2) $ filter even list
+
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 num | odd num = fun2 (num * 3 + 1)
+         | otherwise = sum evens + fun2 firstOdd
+  where
+    evens = takeWhile even $ iterate (\i -> div i 2) num
+    firstOdd = last evens `div` 2
